ef23eaa7f8a9989700f7450bd42da1ef37cb15e8
Sandeepa Ranathunga <sandeeparanatungavcg@gmail.com>
2021-06-22 16:47:50 +0530

    firebase initiation auth and db connection

--- firebase.js
+++ firebase.js
@@ -1,3 +1,7 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
 const firebaseConfig = {
   apiKey: 'AIzaSyBeDAKouPNHxzF7diGQlCXNWdOHxKXf0FI',
   authDomain: 'valkrie-chatter.firebaseapp.com',
@@ -6,3 +10,15 @@ const firebaseConfig = {
   messagingSenderId: '961634854629',
   appId: '1:961634854629:web:87cb04b9c23bf156f6960c',
 };
+
+let app;
+//prevent continious initiation
+if (!firebase.apps.length) {
+  app = firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app();
+}
+const database = app.firestore();
+const auth = firebase.auth();
+
+export { database, auth };
